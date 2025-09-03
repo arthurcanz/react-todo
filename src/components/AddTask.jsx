2144749d375f594835dc734cea9713829c50d0e6
Arthur Canz <arthur.canzian@arkx.cc>
2025-09-03 20:14:28 -0300

    todo

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Tiltle of the task"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Description of the task"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          // VERIFICAR SE O TITULO E A DESCRICAO ESTAO PREENCHIDOS
+          if (!title.trim() || !description.trim()) {
+            return alert("Fill the title and the task description.");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
